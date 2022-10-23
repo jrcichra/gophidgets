688431973614e413dafc0bc02879b7927937a6d6
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-10-22 20:24:49 -0400

    chore(voltage): add all sensor types

--- phidgets/voltageinput.go
+++ phidgets/voltageinput.go
@@ -10,24 +10,63 @@ void ccallback(void* handle, void* ctx, double b);  // Forward declaration.
 */
 import "C"
 import (
+	"errors"
 	"unsafe"
 
 	gopointer "github.com/mattn/go-pointer"
 )
 
-//PhidgetVoltageInput is the struct that is a phidget voltageinput sensor
+var voltageInputSensorTypeMap map[string]C.PhidgetVoltageInput_SensorType = map[string]C.PhidgetVoltageInput_SensorType{
+	"SENSOR_TYPE_VOLTAGE":  0x0,
+	"SENSOR_TYPE_1114":     0x2b84,
+	"SENSOR_TYPE_1117":     0x2ba2,
+	"SENSOR_TYPE_1123":     0x2bde,
+	"SENSOR_TYPE_1127":     0x2c06,
+	"SENSOR_TYPE_1130_PH":  0x2c25,
+	"SENSOR_TYPE_1130_ORP": 0x2c26,
+	"SENSOR_TYPE_1132":     0x2c38,
+	"SENSOR_TYPE_1133":     0x2c42,
+	"SENSOR_TYPE_1135":     0x2c56,
+	"SENSOR_TYPE_1142":     0x2c9c,
+	"SENSOR_TYPE_1143":     0x2ca6,
+	"SENSOR_TYPE_3500":     0x88b8,
+	"SENSOR_TYPE_3501":     0x88c2,
+	"SENSOR_TYPE_3502":     0x88cc,
+	"SENSOR_TYPE_3503":     0x88d6,
+	"SENSOR_TYPE_3507":     0x88fe,
+	"SENSOR_TYPE_3508":     0x8908,
+	"SENSOR_TYPE_3509":     0x8912,
+	"SENSOR_TYPE_3510":     0x891c,
+	"SENSOR_TYPE_3511":     0x8926,
+	"SENSOR_TYPE_3512":     0x8930,
+	"SENSOR_TYPE_3513":     0x893a,
+	"SENSOR_TYPE_3514":     0x8944,
+	"SENSOR_TYPE_3515":     0x894e,
+	"SENSOR_TYPE_3516":     0x8958,
+	"SENSOR_TYPE_3517":     0x8962,
+	"SENSOR_TYPE_3518":     0x896c,
+	"SENSOR_TYPE_3519":     0x8976,
+	"SENSOR_TYPE_3584":     0x8c00,
+	"SENSOR_TYPE_3585":     0x8c0a,
+	"SENSOR_TYPE_3586":     0x8c14,
+	"SENSOR_TYPE_3587":     0x8c1e,
+	"SENSOR_TYPE_3588":     0x8c28,
+	"SENSOR_TYPE_3589":     0x8c32,
+}
+
+// PhidgetVoltageInput is the struct that is a phidget voltageinput sensor
 type PhidgetVoltageInput struct {
 	phidget
 	handle C.PhidgetVoltageInputHandle
 }
 
-//Create creates a phidget voltageinput sensor
+// Create creates a phidget voltageinput sensor
 func (p *PhidgetVoltageInput) Create() {
 	C.PhidgetVoltageInput_create(&p.handle)
 	p.rawHandle(unsafe.Pointer(p.handle))
 }
 
-//GetValue gets the voltageinput from a phidget voltageinput sensor
+// GetValue gets the voltageinput from a phidget voltageinput sensor
 func (p *PhidgetVoltageInput) GetValue() (float64, error) {
 	var r C.double
 	cerr := C.PhidgetVoltageInput_getVoltage(p.handle, &r)
@@ -37,7 +76,7 @@ func (p *PhidgetVoltageInput) GetValue() (float64, error) {
 	return float64(r), nil
 }
 
-//SetOnVoltageChangeHandler - voltage input for temperature changes calls a function
+// SetOnVoltageChangeHandler - voltage input for temperature changes calls a function
 func (p *PhidgetVoltageInput) SetOnVoltageChangeHandler(f func(float64)) error {
 	//make a c function pointer to a go function pointer and pass it through the phidget context
 	var passthrough Passthrough
@@ -50,7 +89,15 @@ func (p *PhidgetVoltageInput) SetOnVoltageChangeHandler(f func(float64)) error {
 	return nil
 }
 
-//Close - close the handle and delete it
+// SetSensorType - Specific to a voltageinputratio - setting the proper sensor type
+func (p *PhidgetVoltageInput) SetSensorType(sensorType string) error {
+	if sensorCode, ok := voltageInputSensorTypeMap[sensorType]; ok {
+		return p.phidgetError(C.PhidgetVoltageInput_setSensorType(p.handle, sensorCode))
+	}
+	return errors.New("Unknown sensorType: " + sensorType + ". Please add it to the mapping switch in gophidgets voltageinput.go")
+}
+
+// Close - close the handle and delete it
 func (p *PhidgetVoltageInput) Close() error {
 	if err := p.phidget.Close(); err != nil {
 		return err
--- phidgets/voltageinputratio.go
+++ phidgets/voltageinputratio.go
@@ -16,19 +16,71 @@ import (
 	gopointer "github.com/mattn/go-pointer"
 )
 
-//PhidgetVoltageRatioInput is the struct that is a phidget voltageinputratio sensor
+var voltageRatioInputSensorTypeMap map[string]C.PhidgetVoltageRatioInput_SensorType = map[string]C.PhidgetVoltageRatioInput_SensorType{
+	"SENSOR_TYPE_VOLTAGERATIO":      0x0,
+	"SENSOR_TYPE_1101_SHARP_2D120X": 0x2b03,
+	"SENSOR_TYPE_1101_SHARP_2Y0A21": 0x2b04,
+	"SENSOR_TYPE_1101_SHARP_2Y0A02": 0x2b05,
+	"SENSOR_TYPE_1102":              0x2b0c,
+	"SENSOR_TYPE_1103":              0x2b16,
+	"SENSOR_TYPE_1104":              0x2b20,
+	"SENSOR_TYPE_1105":              0x2b2a,
+	"SENSOR_TYPE_1106":              0x2b34,
+	"SENSOR_TYPE_1107":              0x2b3e,
+	"SENSOR_TYPE_1108":              0x2b48,
+	"SENSOR_TYPE_1109":              0x2b52,
+	"SENSOR_TYPE_1110":              0x2b5c,
+	"SENSOR_TYPE_1111":              0x2b66,
+	"SENSOR_TYPE_1112":              0x2b70,
+	"SENSOR_TYPE_1113":              0x2b7a,
+	"SENSOR_TYPE_1115":              0x2b8e,
+	"SENSOR_TYPE_1116":              0x2b98,
+	"SENSOR_TYPE_1118_AC":           0x2bad,
+	"SENSOR_TYPE_1118_DC":           0x2bae,
+	"SENSOR_TYPE_1119_AC":           0x2bb7,
+	"SENSOR_TYPE_1119_DC":           0x2bb8,
+	"SENSOR_TYPE_1120":              0x2bc0,
+	"SENSOR_TYPE_1121":              0x2bca,
+	"SENSOR_TYPE_1122_AC":           0x2bd5,
+	"SENSOR_TYPE_1122_DC":           0x2bd6,
+	"SENSOR_TYPE_1124":              0x2be8,
+	"SENSOR_TYPE_1125_HUMIDITY":     0x2bf3,
+	"SENSOR_TYPE_1125_TEMPERATURE":  0x2bf4,
+	"SENSOR_TYPE_1126":              0x2bfc,
+	"SENSOR_TYPE_1128":              0x2c10,
+	"SENSOR_TYPE_1129":              0x2c1a,
+	"SENSOR_TYPE_1131":              0x2c2e,
+	"SENSOR_TYPE_1134":              0x2c4c,
+	"SENSOR_TYPE_1136":              0x2c60,
+	"SENSOR_TYPE_1137":              0x2c6a,
+	"SENSOR_TYPE_1138":              0x2c74,
+	"SENSOR_TYPE_1139":              0x2c7e,
+	"SENSOR_TYPE_1140":              0x2c88,
+	"SENSOR_TYPE_1141":              0x2c92,
+	"SENSOR_TYPE_1146":              0x2cc4,
+	"SENSOR_TYPE_3120":              0x79e0,
+	"SENSOR_TYPE_3121":              0x79ea,
+	"SENSOR_TYPE_3122":              0x79f4,
+	"SENSOR_TYPE_3123":              0x79fe,
+	"SENSOR_TYPE_3130":              0x7a44,
+	"SENSOR_TYPE_3520":              0x8980,
+	"SENSOR_TYPE_3521":              0x898a,
+	"SENSOR_TYPE_3522":              0x8994,
+}
+
+// PhidgetVoltageRatioInput is the struct that is a phidget voltageinputratio sensor
 type PhidgetVoltageRatioInput struct {
 	phidget
 	handle C.PhidgetVoltageRatioInputHandle
 }
 
-//Create creates a phidget voltageinputratio sensor
+// Create creates a phidget voltageinputratio sensor
 func (p *PhidgetVoltageRatioInput) Create() {
 	C.PhidgetVoltageRatioInput_create(&p.handle)
 	p.rawHandle(unsafe.Pointer(p.handle))
 }
 
-//GetValue gets the voltageinputratio from a phidget voltageinputratio sensor
+// GetValue gets the voltageinputratio from a phidget voltageinputratio sensor
 func (p *PhidgetVoltageRatioInput) GetValue() (float64, error) {
 	var r C.double
 	if cerr := C.PhidgetVoltageRatioInput_getVoltageRatio(p.handle, &r); cerr != C.EPHIDGET_OK {
@@ -37,7 +89,7 @@ func (p *PhidgetVoltageRatioInput) GetValue() (float64, error) {
 	return float64(r), nil
 }
 
-//SetOnVoltageRatioChangeHandler - voltage input changes calls a function
+// SetOnVoltageRatioChangeHandler - voltage input changes calls a function
 func (p *PhidgetVoltageRatioInput) SetOnVoltageRatioChangeHandler(f func(float64)) error {
 	//make a c function pointer to a go function pointer and pass it through the phidget context
 	var passthrough Passthrough
@@ -50,20 +102,15 @@ func (p *PhidgetVoltageRatioInput) SetOnVoltageRatioChangeHandler(f func(float64
 	return nil
 }
 
-//SetSensorType - Specific to a voltageinputratio - setting the proper sensor type
+// SetSensorType - Specific to a voltageinputratio - setting the proper sensor type
 func (p *PhidgetVoltageRatioInput) SetSensorType(sensorType string) error {
-	//TODO: need a better way to select a voltage ratio input sensor type by bringing the enum out to go world
-	var cSensor C.PhidgetVoltageRatioInput_SensorType
-	switch sensorType {
-	case "SENSOR_TYPE_1122_DC":
-		cSensor = C.SENSOR_TYPE_1122_DC
-	default:
-		return errors.New("Unknown sensorType: " + sensorType + ". Please add it to the mapping switch in voltageinputratio.go")
+	if sensorCode, ok := voltageRatioInputSensorTypeMap[sensorType]; ok {
+		return p.phidgetError(C.PhidgetVoltageRatioInput_setSensorType(p.handle, sensorCode))
 	}
-	return p.phidgetError(C.PhidgetVoltageRatioInput_setSensorType(p.handle, cSensor))
+	return errors.New("Unknown sensorType: " + sensorType + ". Please add it to the mapping switch in gophidgets voltageinputratio.go")
 }
 
-//Close - close the handle and delete it
+// Close - close the handle and delete it
 func (p *PhidgetVoltageRatioInput) Close() error {
 	if err := p.phidget.Close(); err != nil {
 		return err
