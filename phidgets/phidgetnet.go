2a59a0f0289c0c7b2fd38ffa5a41fe02f99eb1ef
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2026-03-06 00:19:12 -0500

    Add RemoveServer function to phidget network device

--- phidgets/phidgetnet.go
+++ phidgets/phidgetnet.go
@@ -9,7 +9,7 @@ package phidgets
 import "C"
 import "unsafe"
 
-// AddServer adds a
+// AddServer adds a network server for the phidget library to connect to.
 func AddServer(serverName string, address string, port int, password string, flags int) {
 	serverC := C.CString(serverName)
 	addressC := C.CString(address)
@@ -20,4 +20,11 @@ func AddServer(serverName string, address string, port int, password string, fla
 	C.free(unsafe.Pointer(serverC))
 	C.free(unsafe.Pointer(addressC))
 	C.free(unsafe.Pointer(passwordC))
+}
+
+// RemoveServer removes a previously added network server entry, killing its reconnect thread.
+func RemoveServer(serverName string) {
+	serverC := C.CString(serverName)
+	C.PhidgetNet_removeServer(serverC)
+	C.free(unsafe.Pointer(serverC))
 }
